ac17c28ffa145b378e8e99cc5b52c7baaa5eee53
ThalesAymar <thales_fortes126@hotmail.com>
2026-07-04 22:48:19 -0300

    Update main.c
    main: Agora na primeira passada do código, os parênteses de uma L_instructions são removidos e a instrução adicionada a symbol table;
    
    Adicionada segunda passada. No mento apenas pega as partes dest, comp, e jmp de uma C_instruction

--- main.c
+++ main.c
@@ -10,11 +10,13 @@
 
 int main(int argc, char *argv[]) {
 	FILE *arq = fopen("teste.txt", "r");
+
 	
 	if(arq == NULL){
         printf("Erro ao abrir arquivo\n");
         return 1;
     }
+
 	
 	Parser p;
 	
@@ -46,27 +48,64 @@ int main(int argc, char *argv[]) {
 		
 		
 		switch (type) {
-            case A_COMMAND:
-                printf("%s e A_COMMAND\n", p.currentToken );  // 2 PASSADA PEGAR TUDO MENOS @
-                st.romAddress++;
-                break;
-            case C_COMMAND:
-                printf("%s e C_COMMAND\n", p.currentToken ); // 2 PASSADA PEGAR dest = comp;jump
-                st.romAddress++;
-                break;
             case L_COMMAND:
-                printf("%s e L_COMMAND\n", p.currentToken ); // 1 PASSADA PEGAR TUDO MENOS ()
+
+				strcpy(label, p.currentToken + 1);   // pula o '('
+
+				label[strlen(label) - 1] = '\0';     // remove ')'
+				
                 addEntry(&st, p.currentToken, st.romAddress);
                 break;
             default:
-            	printf("Nao achou" ); 
+            	st.romAddress++; 
                 break;
 		}
 	}
 	
+
+	/**************************************************
+	*												  *
+	*												  *
+	*				  SEGUNDA PASSADA				  *
+	*												  *
+	*												  *
+	**************************************************/
 	
-	for(int i = 0; i < st.count; i++){
-		printf("Symbol: %s , Address: %d , Na posicao: %d \n", st.table[i].symbol, st.table[i].address, i);
+	
+	
+	rewind(arq);
+	st.romAddress = 16; //novas adicoes na tabela comecarao do endereco 16
+	
+	while(fgets(p.currentLine, sizeof(p.currentLine), arq) != NULL){
+		
+		char* comment = strstr(p.currentLine, "//");
+
+		if(comment != NULL){
+    		*comment = '\0';
+		}
+		
+		
+		p.currentToken = strtok(p.currentLine, " \t\n");
+		
+		if(p.currentToken == NULL){
+    		continue;
+		}
+		
+		
+		CommandType type = getCommandType(p.currentToken);
+		
+		CInstruction c;
+		 
+		
+		switch (type) {
+            case A_COMMAND:                                
+                break;
+            case C_COMMAND:
+                c = parseCInstruction(p.currentToken);
+                break;
+            default:
+                break;
+		}
 	}
 		
 	return 0;
